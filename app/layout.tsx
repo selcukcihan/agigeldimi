308bfc1796fc716d2f0090176885a341e5aee150
Selcuk Cihan <selcukcihan@gmail.com>
2025-02-20 18:08:59 +0300

    Stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,40 @@ import type React from "react"; // Import React
 
 const inter = Inter({ subsets: ["latin"] });
 
+const title = "AGI Tespit Testi";
+const description =
+  "AGI'ye ulaşıp ulaşmadığımızı anlamak için bu soruları yanıtla";
+
+const images = [
+  {
+    url: `/agigeldimi.png`,
+    width: 360,
+    height: 360,
+    alt: description,
+  },
+];
+
 export const metadata = {
   title: "AGI Tespit Testi",
   description: "AGI'ye ulaşıp ulaşmadığımızı anlamak için bu soruları yanıtla",
+  image: images[0].url,
+  keywords: ["AGI", "Yapay Zeka", "Test"],
+  twitter: {
+    card: "summary",
+    title,
+    description,
+    creator: "@scihan",
+    site: "@scihan",
+    images,
+  },
+  openGraph: {
+    siteName: title,
+    title,
+    type: "website",
+    url: "https://agigeldimi.selcukcihan.com",
+    description,
+    images,
+  },
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import AGIQuiz from "./agi-quiz"
+import AGIQuiz from "./agi-quiz";
 
 export default function Page() {
   return (
     <div className="container mx-auto p-4 min-h-screen flex items-center justify-center">
       <AGIQuiz />
     </div>
-  )
+  );
 }
-
